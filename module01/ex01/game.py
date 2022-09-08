40b836ca00257cacf2f58642f5ddfc1971f828fe
Zekao <zekao@Zekao.localdomain>
2022-09-08 21:49:35 +0200

    finished ex01

---
+++ module01/ex01/game.py
@@ -0,0 +1,27 @@
+class GotCharacter:
+    first_name = ""
+    is_alive = True
+    def __init__(self, first_name, is_alive):
+        self.first_name = first_name
+        self.is_alive = True
+    
+    
+    def __str__(self):
+        return "Name: {}\nIs alive: {}\n".format(self.first_name, self.is_alive)
+
+class Stark(GotCharacter):
+    """ A class representing the Stark family. Or when bad things happen to good people. """
+    
+    def __init__(self, first_name=None, is_alive=True):
+        super().__init__(first_name=first_name, is_alive=is_alive)
+        self.family_name = "Stark"
+        self.house_words = "Winter is Coming"
+
+    def die(self):
+        self.is_alive = False
+    
+    def ressurect(self):
+        self.is_alive = True
+
+    def print_house_words(self):
+        print(self.house_words)
