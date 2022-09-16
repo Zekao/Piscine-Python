43c9d63e25a595ce684b9240158f10a971859307
Elidjah MAUGALEM <emaugale@e1r7p3.clusters.42paris.fr>
2022-09-17 00:04:36 +0200

    Fixed arbo

--- module02/ex04/setup.py
+++ module02/ex04/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='my_minipack',
     version='1.0.0',
-    packages=['src/my_minipack'],
+    packages=['my_minipack'],
     license='GNU General Public License v3.0',
     author='emaugale',
     author_email='emaugale@student.42.fr',
