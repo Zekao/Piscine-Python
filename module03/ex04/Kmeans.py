24f23a59b3ee1dd5fc400b4a2fbf8eb696c7ca4f
Zekao <Busterk182@gmail.com>
2022-10-30 19:15:51 +0100

    aled

--- module03/ex04/Kmeans.py
+++ module03/ex04/Kmeans.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class KmeansClustering:
-    def __init__(self, max_iter=20, ncentroid=5):
+    def __init__(self, max_iter=20, ncentroid=4):
         self.ncentroid = ncentroid
         self.max_iter = max_iter
         self.centroids = []
@@ -23,11 +23,11 @@ class KmeansClustering:
         -------
         This function should not raise any Exception.
         """
-        if (isinstance(X, np.ndarray) and len(X.shape) == 2):
-            self.centroids = X[np.random.choice(X.shape[0], self.ncentroid, replace=False)]
-            for i in range(self.max_iter):
-                labels = self.predict(X)
-                self.centroids = np.array([X[labels == i].mean(axis=0) for i in range(self.ncentroid)])
+        self.centroids = X[np.random.choice(X.shape[0], self.ncentroid, replace=False)]
+        for i in range(self.max_iter):
+            labels = self.predict(X)
+            self.centroids = np.array([X[labels == i].mean(axis=0) for i in range(self.ncentroid)])
+        print(labels)
         return 
 
     def predict(self, X):
@@ -43,15 +43,19 @@ class KmeansClustering:
         -------
         This function should not raise any Exception.
         """
-        # if (isinstance(X, np.ndarray) and len(X.shape) == 2):
         dist = np.linalg.norm(X[:, np.newaxis, :] - self.centroids[np.newaxis, :, :], axis=2)
         return np.argmin(dist, axis=1)
     
 def main(**kwargs):
     
     kluster = KmeansClustering(**kwargs)
-    data = np.genfromtxt("data.csv", delimiter=",")
+    data = np.genfromtxt("data.csv", delimiter=",", skip_header=1)
+    kluster.fit(data)
+    labels = kluster.predict(data)
+    plt.scatter(data[:, 0], data[:, 1], c=labels)
+    plt.legend(loc="upper left") 
 
+    plt.show()
     
     
 
