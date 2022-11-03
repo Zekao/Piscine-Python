0a035a0109f716c0dd26b12c1833dbc242e401ec
Zekao <Busterk182@gmail.com>
2022-11-04 00:48:04 +0100

    Mais nan enfin fini?

--- module03/ex04/Kmeans.py
+++ module03/ex04/Kmeans.py
@@ -3,12 +3,14 @@ from grapheme import length
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-
+import style
 class KmeansClustering:
-    def __init__(self, max_iter=20, ncentroid=4, filepath=None):
+    def __init__(self, max_iter=20, ncentroid=4, filepath=None, debug="0"):
         self.ncentroid = ncentroid
         self.max_iter = max_iter
         self.centroids = []
+        self.debug = debug
+
 
     def fit(self, X):
         """
@@ -16,16 +18,23 @@ class KmeansClustering:
         For the location of the initial centroids, random pick ncentroids from the dataset.
         Args:
         -----
-        X: has to be an numpy.ndarray, a matrice of dimension m * n.
+
+        X: has to be an numpy.ndarray, apython redoKmeans.py filepath="data.csv" ncentroid=3 max_iter=2 matrice of dimension m * n.
         Return:
         -------
         None.
         Raises:
         -------
         This function should not raise any Exception.
+
+        -------
+        Explains:
+            -   First of all, we will pick a random value for each centroid
+            -   After that, we will use our function predict until we've reached the number max_iter iterations
+            -   
         """
         self.centroids = X[np.random.choice(X.shape[0], self.ncentroid, replace=False)]
-        for i in range(self.max_iter):
+        for _ in range(self.max_iter):
             labels = self.predict(X)
             self.centroids = np.array([X[labels == i].mean(axis=0) for i in range(self.ncentroid)])
         return 
@@ -42,23 +51,38 @@ class KmeansClustering:
         Raises:
         -------
         This function should not raise any Exception.
+
+        ------
+        Explains:
+            -   np.linalg.norm will be useful to calculate distance between many points
+                we will use it to get a tab with the distance between each point
+            -   np.argmin will tell us the position of each lowest value, according to his position we will determine centroid
         """
         dist = np.linalg.norm(X[:, np.newaxis, :] - self.centroids[np.newaxis, :, :], axis=2)
+        if int(self.debug) == 1:
+            print(">" * 30 + " Dist Values " + "<" * 30)
+            print(style.blue(dist))
+            print(">" * 30 + " centroid " + "<" * 30)
+            print(style.blue(np.argmin(dist, axis=1)))
         return np.argmin(dist, axis=1)
     
 def main(**kwargs):
     
-    kluster = KmeansClustering(**kwargs)
-    data = np.genfromtxt(kwargs.get('filepath'), delimiter=",", skip_header=1)
+    try:
+        kluster = KmeansClustering(**kwargs)
+        data = np.genfromtxt(kwargs.get('filepath'), delimiter=",", skip_header=1)
+    except Exception as e:
+        print ('Usage: python Kmeans.py filepath=<filepath> ncentroid=<number> max_iter=<number>')
+        print(style.red('>>> ' + str(e) + ' <<<'))
+        return
 
     for _ in range(0, 20):
+        print('>' * 30 + ' Test ' + str(_) + ' ' + '<' * 30)
         kluster.fit(data)
         labels = kluster.predict(data)
         for i in range(kluster.ncentroid):
             print("Cluster %d: %d points" % (i, np.sum(labels == i)))
-        print ('-' * 120)
-
-
+            print ('-' * 69)
     plt.scatter(data[:, 0], data[:, 1], c=labels)
     plt.legend
     plt.show()
@@ -72,8 +96,3 @@ if __name__ == "__main__":
             if v.isdigit():
                 args[k] = int(v)
     main(**args)
-
-
-
-if __name__ == '__main__':
-    main()
