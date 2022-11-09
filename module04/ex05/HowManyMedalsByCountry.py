81e728afc003cf0a2657fb10ca2eb040664466b8
Elidjah <elidjah@pixelforest.io>
2022-11-09 17:09:07 +0100

    Fixed ex05 but not satisfied...

--- module04/ex05/HowManyMedalsByCountry.py
+++ module04/ex05/HowManyMedalsByCountry.py
@@ -4,15 +4,42 @@ def how_many_medals_by_country(df, name):
     assert isinstance(df, pd.DataFrame), 'df is not a dataframe'
     assert isinstance(name, str), 'name must be a string'
 
-    df_tmp = df.loc[df['Team'] == name]
-    df_tmp = df_tmp[df_tmp['Medal'].notna()]
-    # df_tmp = df_tmp.drop_duplicates(subset = ['Year', 'Sport'])
+    team_sports = [
+            "Basketball",
+            "Football",
+            "Tug-Of-War",
+            "Badminton",
+            "Sailing",
+            "Handball",
+            "Water Polo",
+            "Hockey",
+            "Rowing",
+            "Bobsleigh",
+            "Softball",
+            "Volleyball",
+            "Synchronized Swimming",
+            "Baseball",
+            "Rugby Sevens",
+            "Rugby",
+            "Lacrosse",
+            "Polo",
+    ]
 
-    new_df = df_tmp.groupby(['Year', 'Medal']).size()
+    team_df = df[df["Sport"].isin(team_sports)]
+    team_df = team_df[team_df["Team"] == name]
+    team_df = team_df.dropna(subset=["Medal"])
+    team_df = team_df.drop_duplicates(subset=['Year', 'Medal', 'Event'])
 
-    # return a list of dictionaries
-    dict_medals = [ ]
-    for year in new_df.index.levels[0]:
-        dict_medals.append({'Year': year, 'Gold': 0, 'Silver': 0, 'Bronze': 0})
-        for medal in new_df.index.levels[1]:
-            print(medal, year)
+    solo_df = df[~df["Sport"].isin(team_sports)]
+    solo_df = solo_df[solo_df["Team"] == name]
+    solo_df = solo_df.dropna(subset=["Medal"])
+
+    new_df = pd.concat([team_df, solo_df]).groupby(['Year', 'Medal']).size()
+    new_df = new_df.rename(index = {'Gold': 'G', 'Silver': 'S', 'Bronze': 'B'})
+    new_df = new_df.unstack(level=-1).to_dict(orient='index')
+
+
+    for year in new_df:
+        new_df[year] = {k: new_df[year][k] for k in ['G', 'S', 'B']}
+
+    return new_df
